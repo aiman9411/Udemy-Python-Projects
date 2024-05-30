a6ed8d20109c3c0b58e623b0ec779ce0739641d1
Aiman Nazmi <58276919+aiman9411@users.noreply.github.com>
2024-05-30 23:29:34 +0800

    Add 'while' loop

--- Day 7/hangman.py
+++ Day 7/hangman.py
@@ -1,19 +1,29 @@
 import random
 
-guess = input("Guess a letter: ").lower()
+words = ['jolly', 'mouse', 'lorry']
 
-words = ['computer', 'mouse', 'chair']
-
-def start_game(guess):
-    display = ''
+def start_game():
     choosen_word = random.choice(words)
-    while len(display) != len(choosen_word):
-        display += '_'
+    display = ['_' for i in choosen_word]
+    num_of_life = len(choosen_word)
+
+
+    while '_' in display and num_of_life > 0 :
+        guess = input("Guess a letter: ").lower()
+        
+        if guess in choosen_word:
+            index = [i for i,alphabet in enumerate(choosen_word) if guess == alphabet]
+            for i in index:
+                display[i] = guess
+        else:
+            num_of_life -= 1
+
+        print(''.join(display))
+        print(f'Remaining live: {num_of_life}')
     
+    if '_' not in display:
+        print("Congratulations. You won the game!")
+    else:
+        print("You lose. Play again.")
 
-start_game(guess)
-
-# if guess.isalpha() and len(guess) == 1:
-#     start_game(guess)
-# else:
-#     print("Start Again")
+start_game()
