4ae7acb21fa9b76defe503d53bb338d0a399b205
Aiman Nazmi <58276919+aiman9411@users.noreply.github.com>
2024-06-11 23:24:55 +0800

    Fix bug

--- Day 15/Coffee_machine.py
+++ Day 15/Coffee_machine.py
@@ -25,74 +25,91 @@ MENU = {
     }
 }
 
-
-
 resources = {
     "water": 300,
     "milk": 200,
     "coffee": 100,
 }
 
-def report():
-    print(f"Water: {resources['water']}\nMilk: {resources['milk']}\nCoffee: {resources['coffee']}\nProfit: {profit}")
+def check_zero():
+    for item in resources:
+        if resources[item] <= 0:
+            return False
+    return True
+
 
 
 def start():
-
     profit = 0
-    
-    drink = input("What would you like? (espresso, latte, cappucino):\n")
+    start_mode = True
 
-    def calculate_balance(total_money):
-        balance = total_money - MENU[drink]["cost"]
-        return balance
-    
-    def check_resource(water):
-        for item in MENU:
-            portion = MENU[water]["ingredients"]
-            for sample in portion:
-                if portion[sample] < MENU[water]["ingredients"][sample]:
-                    print("Sorry we have insufficient resources. Kindly come back later")
+    def report():
+        print(f"Water: {resources['water']}\nMilk: {resources['milk']}\nCoffee: {resources['coffee']}\nProfit: {profit}")
+
+    def check_resource(drink):
+        for item in MENU[drink]["ingredients"]:
+            if resources[item] < MENU[drink]["ingredients"][item]:
+                print("Insufficient resource. Please choose another drink")
+                return False
         return True
+    
+    while start_mode:
+        drink = input("What would you like? (espresso, latte, cappuccino):\n")
+        
+        if drink == "off":
+            print("Turning off the coffee machine...")
+            break
+        elif drink == "report":
+            report()
+            continue
+        elif drink not in MENU:
+            print("Please chooese a valid drink")
+            continue
 
-    def update_resources():
-        if check_resource(drink):
+        
+        if not check_resource(drink):
+            continue
+
+        def calculate_balance(total_money):
+            balance = total_money - MENU[drink]["cost"]
+            return balance
+
+        def update_resources():
             for item in resources:
                 resources[item] -= MENU[drink]["ingredients"][item]
-    
-
-    if drink == "report":
-        report()
-
-    else:
-        print("Please insert coins.")
-        quarters = int(input("How many quarters?\n"))
-        dimes = int(input("How many dimes?\n"))
-        nickles = int(input("How many nickles?\n"))
-        pennies = int(input("How many pennies?\n"))
-
-        total = quarters * 0.25 + dimes * 0.10 + nickles * 0.05 + pennies * 0.01
-
-        def check_money(water):
-            if total >= MENU[water]["cost"]:
-                return True
-            else:
-                return False
-
         
-        sufficient_money = check_money(drink)
 
-        if sufficient_money:
-            balance = round(calculate_balance(total),2)
-            check_resource(drink)
-            update_resources()
-            profit += MENU[drink]["cost"]
-            print(profit)
-            print(f"Here is your {balance} in change.")
-            print(f"Here is your {drink}. Enjoy!")
-        
+        if drink == "report":
+            report()
+
         else:
-            print("Insufficient money. Please come back later")
+            print("Please insert coins.")
+            quarters = int(input("How many quarters?\n"))
+            dimes = int(input("How many dimes?\n"))
+            nickles = int(input("How many nickles?\n"))
+            pennies = int(input("How many pennies?\n"))
+
+            total = quarters * 0.25 + dimes * 0.10 + nickles * 0.05 + pennies * 0.01
+
+            def check_money(water):
+                if total >= MENU[water]["cost"]:
+                    return True
+                else:
+                    return False
+
+            sufficient_money = check_money(drink)
+
+            if sufficient_money:
+                balance = round(calculate_balance(total),2)
+                update_resources()
+                profit += MENU[drink]["cost"]
+                print(f"Here is your {balance} in change.")
+                print(f"Here is your {drink} \u2615. Enjoy!")
+                start_mode = check_zero()
+            
+            elif sufficient_money == False:
+                print("Insufficient money. Please come back later.")
+                break
 
 
 start()
