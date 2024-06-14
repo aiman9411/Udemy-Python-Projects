466923471da647fb5529e5478843ace89954ebe4
Aiman Nazmi <58276919+aiman9411@users.noreply.github.com>
2024-06-14 23:46:59 +0800

    Update main.py

--- Day 17/main.py
+++ Day 17/main.py
@@ -1,9 +1,13 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = []
 
 for question in question_data:
     question_bank.append(Question(question["text"], question["answer"]))
 
-print(question_bank[1].question)
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_question():
+    quiz.next_question()
