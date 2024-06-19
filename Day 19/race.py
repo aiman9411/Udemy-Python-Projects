662a88770218459b5dfecb5c7e9f2f262dff6978
Aiman Nazmi <58276919+aiman9411@users.noreply.github.com>
2024-06-19 22:53:17 +0800

    Update race.py

--- Day 19/race.py
+++ Day 19/race.py
@@ -3,8 +3,19 @@ import random
 
 screen = Screen()
 screen.setup(width= 500, height= 400)
-colors = ["red", "green", "blue", "yellow", "purple", "orange", "black", "pink", "brown", "cyan"]
-y_positions = [-70, -40, -10, 20, 50, 80]
+user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win? Red, Green, Blue, Yellow")
+colors = ["red", "green", "blue", "yellow"]
+y_positions = [-70, -40, -10, 20]
+
+finish_line = Turtle()
+finish_line.hideturtle()
+finish_line.penup()
+finish_line.goto(100,200)
+finish_line.pendown()
+finish_line.right(90)
+finish_line.forward(400)
+finish_line.penup()
+
 
 turtle_list = []
 
@@ -16,10 +27,24 @@ for i in range(4):
     turtle_list.append(tim)
 
 def move_turtle(turtles):
-    for i in turtles:
-        i.penup()
-        i.goto(x = 200, y = y_positions[i for i in range(4)])
-
+    race_on = True
+    while race_on:
+        for turtle in turtles:
+            random_distance = random.randint(0,10)
+            turtle.forward(random_distance)
+            if turtle.xcor() > 100:
+                race_on = False
+                winning_color = turtle.pencolor()
+                if winning_color == user_bet:
+                    print(f"{winning_color.capitalize()} turtle won the competition. You win")
+                else:
+                    print(f"{winning_color.capitalize()} turtle won the competition. You lose")
+                
+                word = Turtle()
+                word.hideturtle()
+                word.color("black")
+                word.write(f"{winning_color.capitalize()} turtle wins!", align="center", font=("Arial", 16, "bold"))
 
 move_turtle(turtle_list)
+
 screen.exitonclick()
