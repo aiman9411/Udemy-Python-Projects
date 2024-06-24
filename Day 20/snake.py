00057d6cfc4ec48ae14b139600c686f1c5a3fc3e
Aiman Nazmi <58276919+aiman9411@users.noreply.github.com>
2024-06-24 23:25:25 +0800

    Update snake.py

--- Day 20/snake.py
+++ Day 20/snake.py
@@ -1,29 +1,44 @@
-from turtle import Turtle, Screen
+from turtle import Turtle
+import time
 
-# Screen Setting
-screen = Screen()
-screen.setup(600,600)
-screen.bgcolor("black")
-screen.title("My Snake Game")
-screen.tracer(0)
+X_AXIS = [20, 0, -20]
+STEP = 20
 
-snake_list = []
-x_axis = [-20, 0, 20]
-for i in range(3):
-    snake = Turtle()
-    snake.penup()
-    snake.goto(x_axis[i], 0)
-    snake.shape("square")
-    snake.color("white")
-    snake_list.append(snake)
+class Snake:
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
 
-moving = True
-while moving:
-    for snake in snake_list:                        
-        snake.forward(5)
-        
-    head_snake = snake_list[2]
-    if head_snake.xcor() == 280:
-        moving = False
+    def create_snake(self):
+        for position in X_AXIS:
+            segment = Turtle()
+            segment.shape("square")
+            segment.color("white")
+            segment.penup()
+            segment.goto(position, 0)
+            self.segments.append(segment)
 
-screen.exitonclick()
+    def move_snake(self):
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_xcor = self.segments[seg_num - 1].xcor()
+            new_ycor = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_xcor, new_ycor)
+        self.head.forward(STEP)
+
+    def snake_up(self):
+        if self.head.heading() != 270:
+            self.head.setheading(90)
+
+    def snake_left(self):
+        if self.head.heading() != 0:
+            self.head.setheading(180)
+    
+    def snake_right(self):
+        if self.head.heading() != 180:
+            self.head.setheading(0)
+
+    def snake_down(self):
+        if self.head.heading() != 90:
+            self.head.setheading(270)
+
