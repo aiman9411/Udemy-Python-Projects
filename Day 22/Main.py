0a2927c381f94d341e4c6e7020d8ef3fe967e706
Aiman Nazmi <58276919+aiman9411@users.noreply.github.com>
2024-06-27 23:31:41 +0800

    Update Main.py

--- Day 22/Main.py
+++ Day 22/Main.py
@@ -5,8 +5,18 @@ screen = Screen()
 screen.setup(800, 600)
 screen.bgcolor("black")
 screen.title("My Pong Game")
+screen.tracer(0)
 
+right_paddle = Paddle(position = (350,0))
+left_paddle = Paddle(position = (-350,0))
+screen.listen()
+screen.onkey(right_paddle.go_up, "Up")
+screen.onkey(right_paddle.go_down, "Down")
+screen.onkey(left_paddle.go_up, "w")
+screen.onkey(left_paddle.go_down, "s")
 
-paddle = Paddle()
+game_on = True
+while game_on:
+    screen.update()
 
 screen.exitonclick()
