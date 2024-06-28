e78b55c2274bb9847341d79543c51940e5d7ece0
Aiman Nazmi <58276919+aiman9411@users.noreply.github.com>
2024-06-28 23:40:22 +0800

    Update Main.py

--- Day 22/Main.py
+++ Day 22/Main.py
@@ -1,5 +1,6 @@
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
 
 screen = Screen()
 screen.setup(800, 600)
@@ -9,6 +10,8 @@ screen.tracer(0)
 
 right_paddle = Paddle(position = (350,0))
 left_paddle = Paddle(position = (-350,0))
+ball = Ball()
+
 screen.listen()
 screen.onkey(right_paddle.go_up, "Up")
 screen.onkey(right_paddle.go_down, "Down")
@@ -18,5 +21,9 @@ screen.onkey(left_paddle.go_down, "s")
 game_on = True
 while game_on:
     screen.update()
+    ball.move_ball()
+
+    if (ball.ycor() > 290 or ball.ycor() < -290):
+        ball.bounce()
 
 screen.exitonclick()
