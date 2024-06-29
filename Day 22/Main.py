ebd6ce0f457565d5c71367395352ed73dc3a4536
Aiman Nazmi <58276919+aiman9411@users.noreply.github.com>
2024-06-29 10:58:34 +0800

    Update Main.py

--- Day 22/Main.py
+++ Day 22/Main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from paddle import Paddle
 from ball import Ball
+import time
 
 screen = Screen()
 screen.setup(800, 600)
@@ -20,10 +21,17 @@ screen.onkey(left_paddle.go_down, "s")
 
 game_on = True
 while game_on:
+    time.sleep(0.01)
     screen.update()
     ball.move_ball()
 
     if (ball.ycor() > 290 or ball.ycor() < -290):
-        ball.bounce()
+        ball.bounce_y()
+
+    if ball.distance(right_paddle) < 50 and ball.xcor() > 330:
+        ball.bounce_x()
+
+    if ball.distance(left_paddle) < 50 and ball.xcor() < -330:
+        ball.bounce_x()
 
 screen.exitonclick()
