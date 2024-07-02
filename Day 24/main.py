805d02939cb6af0172bfb18965600ce081fde343
Aiman Nazmi <58276919+aiman9411@users.noreply.github.com>
2024-07-02 23:19:54 +0800

    Add files via upload

---
+++ Day 24/main.py
@@ -0,0 +1,46 @@
+from turtle import Screen
+from snake import Snake
+from food import Food
+from score import Score
+import time
+
+# Screen Setting
+screen = Screen()
+screen.setup(600,600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+score = Score()
+
+screen.listen()
+screen.onkey(snake.snake_up, "Up")
+screen.onkey(snake.snake_left, "Left")
+screen.onkey(snake.snake_right, "Right")
+screen.onkey(snake.snake_down, "Down")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move_snake()
+
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        score.increase_score()
+        snake.extend()
+
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            score.reset()
+            snake.reset()
+
+    if (snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280):
+        score.reset()
+        snake.reset()
+
+screen.exitonclick()
---
+++ Day 24/score.py
@@ -0,0 +1,28 @@
+from turtle import Turtle
+
+class Score(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        with open("data.txt", mode="r") as file:
+            self.high_score = int(file.read())
+        self.color("white")
+        self.penup()
+        self.goto(0, 270)
+        self.update_score()
+        self.hideturtle()
+    
+    def update_score(self):
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align="center", font=("Arial", 24, "normal"))
+
+    def increase_score(self):
+        self.score += 1
+        self.update_score()
+
+    def reset(self):
+        if self.score > self.high_score:
+            with open("data.txt", mode="w") as file:
+                file.write(f"{self.high_score}")
+        self.score = 0
+        self.update_score()
