64eda1150709fc6e38e211706f2405409c3eae8b
Aiman Nazmi <58276919+aiman9411@users.noreply.github.com>
2024-07-06 20:19:40 +0800

    Update main.py

--- Day 25/main.py
+++ Day 25/main.py
@@ -1,4 +1,31 @@
 from screen_setup import Screen
+import pandas as pd
+
+data = pd.read_csv("50_states.csv")
+states = data["state"].to_list()
+
+def get_x(word):
+    x_loc = data[data['state'] == word]['x'].iloc[0]
+    return x_loc
+
+def get_y(word):
+    y_loc = data[data['state'] == word]['y'].iloc[0]
+    return y_loc
 
 screen = Screen()
+play_game = True
+
+while play_game:
+    answer = screen.ask_question()
+    if answer in states:
+        screen.number += 1
+        x_pos = get_x(answer)
+        y_pos = get_y(answer)
+        screen.create_label(x_pos, y_pos, answer)
+    else:
+        play_game = False
+        screen.total_score()
+        
 screen.run()
+
+
