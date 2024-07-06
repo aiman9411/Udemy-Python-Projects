0f7cd3105e46ff838198f206b787c724c3156142
Aiman Nazmi <58276919+aiman9411@users.noreply.github.com>
2024-07-06 23:08:18 +0800

    Update main.py

--- Day 25/main.py
+++ Day 25/main.py
@@ -3,6 +3,8 @@ import pandas as pd
 
 data = pd.read_csv("50_states.csv")
 states = data["state"].to_list()
+guess_state = []
+missing_state = []
 
 def get_x(word):
     x_loc = data[data['state'] == word]['x'].iloc[0]
@@ -19,13 +21,17 @@ while play_game:
     answer = screen.ask_question()
     if answer in states:
         screen.number += 1
+        guess_state.append(answer)
         x_pos = get_x(answer)
         y_pos = get_y(answer)
         screen.create_label(x_pos, y_pos, answer)
     else:
         play_game = False
         screen.total_score()
+        for state in states:
+            if state not in guess_state:
+                missing_state.append(state)
+        new_data = pd.DataFrame(missing_state)
+        new_data.to_csv("to_review.csv")
         
 screen.run()
-
-
